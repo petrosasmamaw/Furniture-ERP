7f01387e2c12cd0a52ed18357f2f46f7ffe759cc
Petros Asmamaw <petrosasmamaw2@gmail.com>
2026-02-25 21:53:34 +0300

    update navbar

--- adminfrontend/src/pages/navbar.jsx
+++ adminfrontend/src/pages/navbar.jsx
@@ -12,13 +12,13 @@ const Navbar = () => {
           </Link>
           <ul className="navbar-menu">
             <li><Link to="/dashboard" className="navbar-link"><i className="fas fa-tachometer-alt"></i> Dashboard</Link></li>
+                        <li><Link to="/products" className="navbar-link"><i className="fas fa-chair"></i> Products</Link></li>
             <li><Link to="/balances" className="navbar-link"><i className="fas fa-money-bill-wave"></i> Balances</Link></li>
             <li><Link to="/credits" className="navbar-link"><i className="fas fa-credit-card"></i> Credits</Link></li>
             <li><Link to="/items" className="navbar-link"><i className="fas fa-box"></i> Items</Link></li>
             <li><Link to="/machines" className="navbar-link"><i className="fas fa-cogs"></i> Machines</Link></li>
             <li><Link to="/orders" className="navbar-link"><i className="fas fa-shopping-cart"></i> Orders</Link></li>
             <li><Link to="/purchases" className="navbar-link"><i className="fas fa-shopping-bag"></i> Purchases</Link></li>
-            <li><Link to="/products" className="navbar-link"><i className="fas fa-chair"></i> Products</Link></li>
             <li><Link to="/price-calculator" className="navbar-link"><i className="fas fa-calculator"></i> Price#</Link></li>
             <li><Link to="/workers" className="navbar-link"><i className="fas fa-users"></i> Workers</Link></li>
           </ul>
